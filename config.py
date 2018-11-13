4c2cf68850147d44db038f9001177f142568a95c
Cesar Domingos <cesar_web@live.com>
2018-11-13 08:50:10 -0200

    Curso de Flask Microframework - Parte 2

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+SECRET_KEY = 'Cesar'
+MYSQL_HOST = "127.0.0.1"
+MYSQL_PORT = 3306
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "root"
+MYSQL_DB = "jogoteca"
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
--- jogoteca.py
+++ jogoteca.py
@@ -1,84 +1,12 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.secret_key = 'Cesar'
+app.config.from_pyfile('config.py')
 
+db = MySQL(app)
 
-class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
+from views import *
 
-
-class Usuario:
-    def __init__(self, id, nome, senha):
-        self.id = id
-        self.nome = nome
-        self.senha = senha
-
-
-usuario1 = Usuario("cesar", "Cesar", "123")
-usuario2 = Usuario("flora", "Flora", "321")
-usuario3 = Usuario("henry", "Henry", "111")
-usuarios = {usuario1.id: usuario1, usuario2.id: usuario2, usuario3.id: usuario3}
-
-jogo1 = Jogo("Super Mario", "Ação", "SNES")
-jogo2 = Jogo("Pokemon Gold", "RPG", "GBA")
-jogo3 = Jogo("Mortal Kombat", "Luta", "SNES")
-lista = [jogo1, jogo2, jogo3]
-
-
-@app.route("/")
-def index():
-    return render_template('lista.html', titulo="jogos", jogos=lista)
-
-
-@app.route("/novo")
-def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo="Novo Jogo")
-
-
-@app.route("/criar", methods=['POST'])
-def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
-    return redirect(url_for('index'))
-
-
-@app.route("/login")
-def login():
-    proxima = request.args.get('proxima')
-    return render_template("login.html", proxima=proxima)
-
-
-@app.route("/autenticar", methods=["POST"])
-def autenticar():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[request.form['usuario']]
-        if usuario.senha == request.form["senha"]:
-            session['usuario_logado'] = usuario.id
-            flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-        else:
-            flash('Senha inválido!')
-            return redirect(url_for('login'))
-    else:
-        flash('Usuário inválido!')
-        return redirect(url_for('login'))
-
-
-@app.route("/logout")
-def logout():
-    session['usuario_logado'] = None
-    flash("Deslogado com sucesso!")
-    return redirect(url_for('index'))
-
-
-app.run(port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
